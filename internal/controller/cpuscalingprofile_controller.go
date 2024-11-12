94ce78ca23b2f15ceb688b83bc72b7356d7c3328
Martin Hiner <martin.hiner@amd.com>
2024-11-12 15:27:01 +0000

    Implement logic in CPUScalingProfile

--- internal/controller/cpuscalingprofile_controller.go
+++ internal/controller/cpuscalingprofile_controller.go
@@ -18,12 +18,15 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	powerv1 "github.com/intel/kubernetes-power-manager/api/v1"
 )
@@ -49,11 +52,175 @@ type CPUScalingProfileReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *CPUScalingProfileReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	var err error
+	logger := r.Log.WithValues("scalingprofile", req.NamespacedName)
+	if req.Namespace != IntelPowerNamespace {
+		err := fmt.Errorf("incorrect namespace")
+		logger.Error(err, "resource is not in the intel-power namespace, ignoring")
+		return ctrl.Result{Requeue: false}, err
+	}
+	logger.Info("reconciling the scaling profile")
 
-	// TODO(user): your logic here
+	scalingProfile := &powerv1.CPUScalingProfile{}
+	defer func() { _ = writeUpdatedStatusErrsIfRequired(ctx, r.Status(), scalingProfile, err) }()
+	err = r.Client.Get(context.TODO(), req.NamespacedName, scalingProfile)
+	logger.V(5).Info("retrieving the scaling profile instances")
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// perform clean up on our side and PowerProfile side
+			err = r.deletePowerProfile(&req)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+
+			return ctrl.Result{}, nil
+		}
+
+		// Requeue the request
+		return ctrl.Result{}, err
+	}
+
+	// verify received params
+	logger.V(5).Info("making sure max value is higher than the min value")
+	if scalingProfile.Spec.Max < scalingProfile.Spec.Min {
+		maxLowerThanMinError := errors.NewServiceUnavailable("max scaling treshold value cannot be lower than the min value")
+		logger.Error(maxLowerThanMinError, fmt.Sprintf("error creating the profile '%s'", scalingProfile.Spec.Name))
+		return ctrl.Result{Requeue: false}, maxLowerThanMinError
+	}
+
+	// pass relevant values to associated PowerProfile
+	err = r.ensurePowerProfile(
+		&req,
+		scalingProfile.Spec.Min,
+		scalingProfile.Spec.Min,
+		scalingProfile.Spec.Epp,
+	)
+	if err != nil {
+		logger.Error(err, "")
+		return ctrl.Result{}, err
+	}
+
+	// pass params to CPUPerformanceScalingWorker
 
 	return ctrl.Result{}, nil
+}
+
+// create or update associated PowerProfile
+func (r *CPUScalingProfileReconciler) ensurePowerProfile(req *ctrl.Request, min int, max int, epp string) error {
+	var err error
+	logger := r.Log.WithValues("scalingprofile", req.NamespacedName)
+	nodeName := os.Getenv("NODE_NAME")
+
+	powerProfileName := fmt.Sprintf("%s-%s", req.NamespacedName.Name, nodeName)
+	powerProfile := &powerv1.PowerProfile{}
+	err = r.Client.Get(context.TODO(), client.ObjectKey{
+		Name:      powerProfileName,
+		Namespace: req.NamespacedName.Namespace,
+	}, powerProfile)
+	if err != nil {
+		if !errors.IsNotFound(err) {
+			logger.Error(err, fmt.Sprintf("error retrieving the power profile '%s'", powerProfileName))
+			return err
+		}
+
+		err = r.createPowerProfile(
+			powerProfileName,
+			req.NamespacedName.Namespace,
+			min,
+			max,
+			epp,
+		)
+		if err != nil {
+			logger.Error(err, fmt.Sprintf("error creating the power profile '%s'", powerProfileName))
+			return err
+		}
+		logger.V(5).Info("power profile successfully created", "name", powerProfileName)
+	} else {
+		wasUpdated, err := r.updatePowerProfile(powerProfile, min, max, epp)
+		if !wasUpdated {
+			logger.V(5).Info("power profile is already in desired stated", "name", powerProfileName)
+		} else {
+			if err != nil {
+				logger.Error(err, fmt.Sprintf("error updating the power profile '%s'", powerProfileName))
+				return err
+			} else {
+				logger.V(5).Info("power profile successfully updated", "name", powerProfileName)
+			}
+		}
+	}
+
+	return nil
+}
+
+// create the PowerProfile
+func (r *CPUScalingProfileReconciler) createPowerProfile(name string, namespace string, min int, max int, epp string) error {
+	powerProfile := &powerv1.PowerProfile{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	powerProfileSpec := &powerv1.PowerProfileSpec{
+		Name:     name,
+		Shared:   true,
+		Max:      max,
+		Min:      min,
+		Epp:      epp,
+		Governor: "userspace",
+	}
+	powerProfile.Spec = *powerProfileSpec
+
+	return r.Client.Create(context.TODO(), powerProfile)
+}
+
+// check if update is needed and update the PowerProfile
+func (r *CPUScalingProfileReconciler) updatePowerProfile(powerProfile *powerv1.PowerProfile, min int, max int, epp string) (bool, error) {
+	if powerProfile.Spec.Min == min &&
+		powerProfile.Spec.Max == max &&
+		powerProfile.Spec.Epp == epp {
+		return false, nil
+	}
+
+	powerProfileSpec := &powerv1.PowerProfileSpec{
+		Name:     powerProfile.Spec.Name,
+		Shared:   true,
+		Max:      max,
+		Min:      min,
+		Epp:      epp,
+		Governor: "userspace",
+	}
+	powerProfile.Spec = *powerProfileSpec
+
+	return true, r.Client.Update(context.TODO(), powerProfile)
+}
+
+// send delete request to associated PowerProfile
+func (r *CPUScalingProfileReconciler) deletePowerProfile(req *ctrl.Request) error {
+	var err error
+	logger := r.Log.WithValues("scalingprofile", req.NamespacedName)
+	nodeName := os.Getenv("NODE_NAME")
+
+	powerProfileName := fmt.Sprintf("%s-%s", req.NamespacedName.Name, nodeName)
+	powerProfile := &powerv1.PowerProfile{}
+	err = r.Client.Get(context.TODO(), client.ObjectKey{
+		Name:      powerProfileName,
+		Namespace: req.NamespacedName.Namespace,
+	}, powerProfile)
+	if err != nil {
+		if !errors.IsNotFound(err) {
+			logger.Error(err, fmt.Sprintf("error deleting the power profile '%s' from the cluster", powerProfileName))
+			return err
+		}
+	} else {
+		err = r.Client.Delete(context.TODO(), powerProfile)
+		if err != nil {
+			logger.Error(err, fmt.Sprintf("error deleting the power profile '%s' from the cluster", powerProfileName))
+			return err
+		}
+	}
+
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
--- internal/controller/cpuscalingprofile_controller_test.go
+++ internal/controller/cpuscalingprofile_controller_test.go
@@ -1,6 +1,4 @@
 /*
-
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -18,67 +16,494 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap/zapcore"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/config"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	powerv1 "github.com/intel/kubernetes-power-manager/api/v1"
+	"github.com/intel/kubernetes-power-manager/pkg/testutils"
 )
 
-var _ = Describe("CPUScalingProfile Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+type PowerProfileParams struct {
+	min int
+	max int
+	epp string
+}
 
-		ctx := context.Background()
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		cpuscalingprofile := &powerv1.CPUScalingProfile{}
+func createScalingProfileReconcilerObject(objs []runtime.Object) (*CPUScalingProfileReconciler, error) {
+	log.SetLogger(zap.New(
+		zap.UseDevMode(true),
+		func(opts *zap.Options) {
+			opts.TimeEncoder = zapcore.ISO8601TimeEncoder
+		},
+	),
+	)
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind CPUScalingProfile")
-			err := k8sClient.Get(ctx, typeNamespacedName, cpuscalingprofile)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &powerv1.CPUScalingProfile{
+	// Add route Openshift scheme
+	if err := powerv1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+
+	// Create a fake client to mock API calls.
+	cl := fake.NewClientBuilder().WithRuntimeObjects(objs...).WithScheme(s).Build()
+
+	// Create a ReconcileNode object with the scheme and fake client.
+	r := &CPUScalingProfileReconciler{cl, ctrl.Log.WithName("testing"), s}
+
+	return r, nil
+}
+
+func TestCPUPerformanceScalingProfile_Wrong_Namespace(t *testing.T) {
+	r, err := createScalingProfileReconcilerObject([]runtime.Object{})
+	assert.Nil(t, err)
+	req := reconcile.Request{
+		NamespacedName: client.ObjectKey{
+			Name:      "shared",
+			Namespace: "wrong-namespace",
+		},
+	}
+
+	_, err = r.Reconcile(context.TODO(), req)
+	assert.ErrorContains(t, err, "incorrect namespace")
+}
+
+func TestCPUPerformanceScalingProfile_Reconcile_SetupPass(t *testing.T) {
+	r, err := createProfileReconcilerObject([]runtime.Object{})
+	assert.Nil(t, err)
+	mgr := new(testutils.MgrMock)
+	mgr.On("GetControllerOptions").Return(config.Controller{})
+	mgr.On("GetScheme").Return(r.Scheme)
+	mgr.On("GetLogger").Return(r.Log)
+	mgr.On("SetFields", mock.Anything).Return(nil)
+	mgr.On("Add", mock.Anything).Return(nil)
+	mgr.On("GetCache").Return(new(testutils.CacheMk))
+	err = (&CPUScalingProfileReconciler{
+		Client: r.Client,
+		Scheme: r.Scheme,
+	}).SetupWithManager(mgr)
+	assert.Nil(t, err)
+}
+
+func TestCPUPerformanceScalingProfile_Reconcile_SetupFail(t *testing.T) {
+	r, err := createProfileReconcilerObject([]runtime.Object{})
+	assert.Nil(t, err)
+	mgr := new(testutils.MgrMock)
+	mgr.On("GetControllerOptions").Return(config.Controller{})
+	mgr.On("GetScheme").Return(r.Scheme)
+	mgr.On("GetLogger").Return(r.Log)
+	mgr.On("Add", mock.Anything).Return(fmt.Errorf("setup fail"))
+
+	err = (&PowerProfileReconciler{
+		Client: r.Client,
+		Scheme: r.Scheme,
+	}).SetupWithManager(mgr)
+	assert.Error(t, err)
+}
+
+func TestCPUPerformanceScalingProfile_ensurePowerProfile(t *testing.T) {
+	tcases := []struct {
+		testCase    string
+		nodeName    string
+		profileName string
+		params      PowerProfileParams
+		validateErr func(e error) bool
+		clientObjs  []runtime.Object
+	}{
+		{
+			testCase:    "Test Case 1 - Create PowerProfile",
+			nodeName:    "TestNode",
+			profileName: "user-created",
+			params: PowerProfileParams{
+				min: 2500,
+				max: 3500,
+				epp: "power",
+			},
+			validateErr: func(e error) bool {
+				return assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{},
+		},
+		{
+			testCase:    "Test Case 2 - Update PowerProfile",
+			nodeName:    "TestNode",
+			profileName: "user-created",
+			params: PowerProfileParams{
+				min: 2500,
+				max: 3500,
+				epp: "power",
+			},
+			validateErr: func(e error) bool {
+				return assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{
+				&powerv1.PowerProfile{
 					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
+						Name:      "user-created-TestNode",
+						Namespace: IntelPowerNamespace,
 					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
+					Spec: powerv1.PowerProfileSpec{
+						Name:     "user-created-TestNode",
+						Shared:   true,
+						Min:      2500,
+						Max:      3500,
+						Epp:      "power",
+						Governor: "userspace",
+					},
+				},
+			},
+		},
+	}
+	for _, tc := range tcases {
+		t.Setenv("NODE_NAME", tc.nodeName)
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &powerv1.CPUScalingProfile{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+		r, err := createScalingProfileReconcilerObject(tc.clientObjs)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error creating the reconciler object", tc.testCase)
+		}
 
-			By("Cleanup the specific resource instance CPUScalingProfile")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &CPUScalingProfileReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
+		req := reconcile.Request{
+			NamespacedName: client.ObjectKey{
+				Name:      tc.profileName,
+				Namespace: IntelPowerNamespace,
+			},
+		}
 
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
-	})
-})
+		err = r.ensurePowerProfile(
+			&req,
+			tc.params.min,
+			tc.params.max,
+			tc.params.epp,
+		)
+		tc.validateErr(err)
+
+		powerProfile := &powerv1.PowerProfile{}
+		err = r.Client.Get(context.TODO(), client.ObjectKey{
+			Name:      tc.profileName + "-" + tc.nodeName,
+			Namespace: IntelPowerNamespace,
+		}, powerProfile)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error retrieving the power profile object after testing", tc.testCase)
+		}
+
+		assert.Equal(t, tc.params.min, powerProfile.Spec.Min)
+		assert.Equal(t, tc.params.max, powerProfile.Spec.Max)
+		assert.Equal(t, tc.params.epp, powerProfile.Spec.Epp)
+		assert.Equal(t, true, powerProfile.Spec.Shared)
+		assert.Equal(t, "userspace", powerProfile.Spec.Governor)
+	}
+}
+
+func TestCPUPerformanceScalingProfile_createPowerProfile(t *testing.T) {
+	tcases := []struct {
+		testCase    string
+		nodeName    string
+		profileName string
+		params      PowerProfileParams
+		validateErr func(e error) bool
+		clientObjs  []runtime.Object
+	}{
+		{
+			testCase:    "Test Case 1 - Create PowerProfile with parameters",
+			nodeName:    "TestNode",
+			profileName: "user-created-TestNode",
+			params: PowerProfileParams{
+				min: 2500,
+				max: 3500,
+				epp: "power",
+			},
+			validateErr: func(e error) bool {
+				return assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{},
+		},
+	}
+	for _, tc := range tcases {
+		t.Setenv("NODE_NAME", tc.nodeName)
+
+		r, err := createScalingProfileReconcilerObject(tc.clientObjs)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error creating the reconciler object", tc.testCase)
+		}
+
+		err = r.createPowerProfile(
+			tc.profileName,
+			IntelPowerNamespace,
+			tc.params.min,
+			tc.params.max,
+			tc.params.epp,
+		)
+		tc.validateErr(err)
+
+		powerProfile := &powerv1.PowerProfile{}
+		err = r.Client.Get(context.TODO(), client.ObjectKey{
+			Name:      tc.profileName,
+			Namespace: IntelPowerNamespace,
+		}, powerProfile)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error retrieving the power profile object - should be present", tc.testCase)
+		}
+
+		assert.Equal(t, tc.params.min, powerProfile.Spec.Min)
+		assert.Equal(t, tc.params.max, powerProfile.Spec.Max)
+		assert.Equal(t, tc.params.epp, powerProfile.Spec.Epp)
+		assert.Equal(t, true, powerProfile.Spec.Shared)
+		assert.Equal(t, "userspace", powerProfile.Spec.Governor)
+	}
+}
+
+func TestCPUPerformanceScalingProfile_updatePowerProfile(t *testing.T) {
+	tcases := []struct {
+		testCase    string
+		nodeName    string
+		profileName string
+		params      PowerProfileParams
+		validateErr func(changed bool, e error) bool
+		clientObjs  []runtime.Object
+	}{
+		{
+			testCase:    "Test Case 1 - Same parameters",
+			nodeName:    "TestNode",
+			profileName: "user-created-TestNode",
+			params: PowerProfileParams{
+				min: 2500,
+				max: 3500,
+				epp: "power",
+			},
+			validateErr: func(changed bool, e error) bool {
+				return assert.False(t, changed) && assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{
+				&powerv1.PowerProfile{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "user-created-TestNode",
+						Namespace: IntelPowerNamespace,
+					},
+					Spec: powerv1.PowerProfileSpec{
+						Name:     "user-created-TestNode",
+						Shared:   true,
+						Min:      2500,
+						Max:      3500,
+						Epp:      "power",
+						Governor: "userspace",
+					},
+				},
+			},
+		},
+		{
+			testCase:    "Test Case 2 - Different min",
+			nodeName:    "TestNode",
+			profileName: "user-created-TestNode",
+			params: PowerProfileParams{
+				min: 3000,
+				max: 3500,
+				epp: "power",
+			},
+			validateErr: func(changed bool, e error) bool {
+				return assert.True(t, changed) && assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{
+				&powerv1.PowerProfile{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "user-created-TestNode",
+						Namespace: IntelPowerNamespace,
+					},
+					Spec: powerv1.PowerProfileSpec{
+						Name:     "user-created-TestNode",
+						Shared:   true,
+						Min:      2500,
+						Max:      3500,
+						Epp:      "power",
+						Governor: "userspace",
+					},
+				},
+			},
+		},
+		{
+			testCase:    "Test Case 3 - Different max",
+			nodeName:    "TestNode",
+			profileName: "user-created-TestNode",
+			params: PowerProfileParams{
+				min: 2500,
+				max: 3000,
+				epp: "power",
+			},
+			validateErr: func(changed bool, e error) bool {
+				return assert.True(t, changed) && assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{
+				&powerv1.PowerProfile{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "user-created-TestNode",
+						Namespace: IntelPowerNamespace,
+					},
+					Spec: powerv1.PowerProfileSpec{
+						Name:     "user-created-TestNode",
+						Shared:   true,
+						Min:      2500,
+						Max:      3500,
+						Epp:      "power",
+						Governor: "userspace",
+					},
+				},
+			},
+		},
+		{
+			testCase:    "Test Case 4 - Different EPP",
+			nodeName:    "TestNode",
+			profileName: "user-created-TestNode",
+			params: PowerProfileParams{
+				min: 2500,
+				max: 3500,
+				epp: "performance",
+			},
+			validateErr: func(changed bool, e error) bool {
+				return assert.True(t, changed) && assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{
+				&powerv1.PowerProfile{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "user-created-TestNode",
+						Namespace: IntelPowerNamespace,
+					},
+					Spec: powerv1.PowerProfileSpec{
+						Name:     "user-created-TestNode",
+						Shared:   true,
+						Min:      2500,
+						Max:      3500,
+						Epp:      "power",
+						Governor: "userspace",
+					},
+				},
+			},
+		},
+	}
+	for _, tc := range tcases {
+		t.Setenv("NODE_NAME", tc.nodeName)
+
+		r, err := createScalingProfileReconcilerObject(tc.clientObjs)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error creating the reconciler object", tc.testCase)
+		}
+
+		powerProfile := &powerv1.PowerProfile{}
+		err = r.Client.Get(context.TODO(), client.ObjectKey{
+			Name:      tc.profileName,
+			Namespace: IntelPowerNamespace,
+		}, powerProfile)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error retrieving the power profile object before testing", tc.testCase)
+		}
+
+		changed, err := r.updatePowerProfile(
+			powerProfile,
+			tc.params.min,
+			tc.params.max,
+			tc.params.epp,
+		)
+		tc.validateErr(changed, err)
+
+		err = r.Client.Get(context.TODO(), client.ObjectKey{
+			Name:      tc.profileName,
+			Namespace: IntelPowerNamespace,
+		}, powerProfile)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error retrieving the power profile object after testing", tc.testCase)
+		}
+
+		assert.Equal(t, tc.params.min, powerProfile.Spec.Min)
+		assert.Equal(t, tc.params.max, powerProfile.Spec.Max)
+		assert.Equal(t, tc.params.epp, powerProfile.Spec.Epp)
+		assert.Equal(t, true, powerProfile.Spec.Shared)
+		assert.Equal(t, "userspace", powerProfile.Spec.Governor)
+	}
+}
+
+func TestCPUPerformanceScalingProfile_deletePowerProfile(t *testing.T) {
+	tcases := []struct {
+		testCase    string
+		nodeName    string
+		profileName string
+		validateErr func(e error) bool
+		clientObjs  []runtime.Object
+	}{
+		{
+			testCase:    "Test Case 1 - No PowerProfile to delete",
+			nodeName:    "TestNode",
+			profileName: "user-created",
+			validateErr: func(e error) bool {
+				return assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{},
+		},
+		{
+			testCase:    "Test Case 2 - Successful PowerProfile delete",
+			nodeName:    "TestNode",
+			profileName: "user-created",
+			validateErr: func(e error) bool {
+				return assert.Nil(t, e)
+			},
+			clientObjs: []runtime.Object{
+				&powerv1.PowerProfile{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "user-created-TestNode",
+						Namespace: IntelPowerNamespace,
+					},
+					Spec: powerv1.PowerProfileSpec{},
+				},
+			},
+		},
+	}
+	for _, tc := range tcases {
+		t.Setenv("NODE_NAME", tc.nodeName)
+
+		r, err := createScalingProfileReconcilerObject(tc.clientObjs)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error creating the reconciler object", tc.testCase)
+		}
+
+		req := reconcile.Request{
+			NamespacedName: client.ObjectKey{
+				Name:      tc.profileName,
+				Namespace: IntelPowerNamespace,
+			},
+		}
+
+		err = r.deletePowerProfile(&req)
+		tc.validateErr(err)
+
+		powerProfiles := &powerv1.PowerProfileList{}
+		err = r.Client.List(context.TODO(), powerProfiles)
+		if err != nil {
+			t.Error(err)
+			t.Fatalf("%s - error retrieving the power profile objects", tc.testCase)
+		}
+		if len(powerProfiles.Items) > 0 {
+			t.Errorf("%s - failed: expected the number of power profile objects to be zero", tc.testCase)
+		}
+	}
+}
