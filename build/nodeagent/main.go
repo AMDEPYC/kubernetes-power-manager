7acc791a162abec8c2d9698ff2c59375bc439481
Rafal Lal <rafallal@amd.com>
2024-11-12 15:29:45 +0000

    CPUScalingProfile fixes
    - Delete CPUScalingProfileController from node-agent
    - Add CPUScalingProfile CRD to manager ClusterRole in helm - was missing
    
    Signed-off-by: Rafal Lal <rafallal@amd.com>

--- build/nodeagent/main.go
+++ build/nodeagent/main.go
@@ -267,13 +267,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Uncore")
 		os.Exit(1)
 	}
-	if err = (&controller.CPUScalingProfileReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "CPUScalingProfile")
-		os.Exit(1)
-	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
