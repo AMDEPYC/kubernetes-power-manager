487f6b21eed96bbab1fa0cb0904eaa0196e91170
Martin Matyas <martin.matyas@gmail.com>
2024-11-25 19:50:27 +0000

    Adding support for cpu frequency set/get
    Signed-off-by: Martin Matyas <martin.matyas@amd.com>

---
+++ internal/scaling/cpu_freq.go
@@ -0,0 +1,80 @@
+package scaling
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+const (
+	userspaceGovernor = "userspace"
+	cpuFreqBasePath   = "/sys/devices/system/cpu/cpu%d/cpufreq"
+)
+
+func getCPUFreqPath(cpu uint, resource string) string {
+	cpuFreqPath := fmt.Sprintf(cpuFreqBasePath, cpu)
+	return filepath.Join(cpuFreqPath, resource)
+}
+
+var getCPUFreqPathFunction = getCPUFreqPath
+
+// get current governor
+func getCurrentGovernor(cpu uint) (string, error) {
+	governorPath := getCPUFreqPathFunction(cpu, "scaling_governor")
+
+	currentGovernor, err := os.ReadFile(governorPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read current governor for cpu %d: %w", cpu, err)
+	}
+	return strings.TrimSpace(string(currentGovernor)), nil
+}
+
+func isUserspaceGovernor(cpu uint) (bool, error) {
+	governor, err := getCurrentGovernor(cpu)
+	if err != nil {
+		return false, fmt.Errorf("failed to read current governor for cpu %d: %w", cpu, err)
+	}
+	return governor == userspaceGovernor, nil
+}
+
+// setCPUFrequency sets the CPU frequency in kHz for the specified CPU using the userspace governor.
+func setCPUFrequency(cpu uint, frequency uint) error {
+	// check that the userspace governor is enabled
+	isUserspace, err := isUserspaceGovernor(cpu)
+	if err != nil {
+		return fmt.Errorf("failed to get userspace governor for CPU %d: %w", cpu, err)
+	}
+
+	if !isUserspace {
+		return fmt.Errorf("userspace governor not set for CPU %d", cpu)
+	}
+
+	scalingSetspeedPath := getCPUFreqPathFunction(cpu, "scaling_setspeed")
+	// Set the desired frequency
+	err = os.WriteFile(scalingSetspeedPath, []byte(fmt.Sprintf("%d", frequency)), 0644)
+	if err != nil {
+		return fmt.Errorf("failed to set frequency for CPU %d: %w", cpu, err)
+	}
+
+	return nil
+}
+
+// getCPUFrequency returns the CPU frequency in kHz for the specified CPU.
+func getCPUFrequency(cpu uint) (uint, error) {
+	scalingGetFreqPath := getCPUFreqPathFunction(cpu, "scaling_cur_freq")
+
+	freqData, err := os.ReadFile(scalingGetFreqPath)
+	if err != nil {
+		return 0, fmt.Errorf("failed to read current frequency for CPU %d: %w", cpu, err)
+	}
+
+	freqStr := strings.TrimSpace(string(freqData))
+	freq, err := strconv.ParseUint(freqStr, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("failed to convert frequency for CPU %d to uint: %w", cpu, err)
+	}
+
+	return uint(freq), nil
+}
---
+++ internal/scaling/cpu_freq_test.go
@@ -0,0 +1,112 @@
+package scaling
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func createTempFile(t *testing.T, dir, content string) string {
+	tempFile, err := os.CreateTemp(dir, "tempfile-")
+	require.NoError(t, err, "failed to create temp file")
+
+	if content != "" {
+		_, err := tempFile.WriteString(content)
+		require.NoError(t, err, "failed to write to temp file")
+	}
+	require.NoError(t, tempFile.Close(), "failed to close temp file")
+
+	return tempFile.Name()
+}
+
+func overrideGetCPUFreqPath(t *testing.T, cpu uint, resource string) string {
+	tempDir := t.TempDir()
+	switch resource {
+	case "scaling_governor":
+		if cpu == 0 {
+			return createTempFile(t, tempDir, "userspace\n")
+		} else {
+			return createTempFile(t, tempDir, "powersave\n")
+		}
+	case "scaling_setspeed":
+		return createTempFile(t, tempDir, "")
+	case "scaling_cur_freq":
+		return createTempFile(t, tempDir, "2000000\n")
+	default:
+		require.Fail(t, "Unexpected resource type")
+	}
+	return ""
+}
+
+func TestGetCurrentGovernorUserspace(t *testing.T) {
+	originalGetCPUFreqPath := getCPUFreqPathFunction
+	getCPUFreqPathFunction = func(cpu uint, resource string) string {
+		return overrideGetCPUFreqPath(t, cpu, resource)
+	}
+	defer func() { getCPUFreqPathFunction = originalGetCPUFreqPath }()
+
+	governor, err := getCurrentGovernor(0)
+	require.NoError(t, err)
+	assert.Equal(t, "userspace", governor)
+}
+
+func TestGetCurrentGovernorPowersave(t *testing.T) {
+	originalGetCPUFreqPath := getCPUFreqPathFunction
+	getCPUFreqPathFunction = func(cpu uint, resource string) string {
+		return overrideGetCPUFreqPath(t, cpu, resource)
+	}
+	defer func() { getCPUFreqPathFunction = originalGetCPUFreqPath }()
+
+	governor, err := getCurrentGovernor(1)
+	require.NoError(t, err)
+	assert.Equal(t, "powersave", governor)
+}
+
+func TestIsUserspaceGovernor(t *testing.T) {
+	originalGetCPUFreqPath := getCPUFreqPathFunction
+	getCPUFreqPathFunction = func(cpu uint, resource string) string {
+		return overrideGetCPUFreqPath(t, cpu, resource)
+	}
+	defer func() { getCPUFreqPathFunction = originalGetCPUFreqPath }()
+
+	isUserspace, err := isUserspaceGovernor(0)
+	require.NoError(t, err)
+	assert.True(t, isUserspace)
+}
+
+func TestIsUserspaceGovernorNegative(t *testing.T) {
+	originalGetCPUFreqPath := getCPUFreqPathFunction
+	getCPUFreqPathFunction = func(cpu uint, resource string) string {
+		return overrideGetCPUFreqPath(t, cpu, resource)
+	}
+	defer func() { getCPUFreqPathFunction = originalGetCPUFreqPath }()
+
+	isUserspace, err := isUserspaceGovernor(1)
+	require.NoError(t, err)
+	assert.False(t, isUserspace)
+}
+
+func TestSetCPUFrequency(t *testing.T) {
+	originalGetCPUFreqPath := getCPUFreqPathFunction
+	getCPUFreqPathFunction = func(cpu uint, resource string) string {
+		return overrideGetCPUFreqPath(t, cpu, resource)
+	}
+	defer func() { getCPUFreqPathFunction = originalGetCPUFreqPath }()
+
+	err := setCPUFrequency(0, 3000000)
+	require.NoError(t, err)
+}
+
+func TestGetCPUFrequency(t *testing.T) {
+	originalGetCPUFreqPath := getCPUFreqPathFunction
+	getCPUFreqPathFunction = func(cpu uint, resource string) string {
+		return overrideGetCPUFreqPath(t, cpu, resource)
+	}
+	defer func() { getCPUFreqPathFunction = originalGetCPUFreqPath }()
+
+	freq, err := getCPUFrequency(0)
+	require.NoError(t, err)
+	assert.Equal(t, uint(2000000), freq)
+}
